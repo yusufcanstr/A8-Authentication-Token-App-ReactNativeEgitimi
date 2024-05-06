bb0ac77ff708e52fbd16361794ccc4ad4e47d946
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-05-06 13:51:20 +0300

    Kullanıcı Kaydetme

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,9 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import AuthContent from "../component.js/AuthContent";
+import { createUser } from "../util/auth";
+
+async function signUpHandler({ email, password }) {
+  await createUser(email, password);
+}
 
 export default function SignupScreen() {
-  return <AuthContent />;
+  return <AuthContent onAuthenticate={signUpHandler} />;
 }
 
 const styles = StyleSheet.create({});
--- util/auth.js
+++ util/auth.js
@@ -3,7 +3,8 @@ import { func } from "prop-types";
 
 const API_KEY = "AIzaSyCMDczPdS4V4Q2JxnrTr21sc0qXIrKzWcM";
 
-async function createUser(email, password) {
+//export dediğimizde başka classlardan da bu classa erişe biliriz
+export async function createUser(email, password) {
   const response = await axios.post(
     "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     {
